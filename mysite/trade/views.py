10b805afb401fc31462be7cb5cee72ff38e0db99
sadolin <sadolintw@gmail.com>
2022-12-13 08:15:52 +0800

    fix: close open orders before get usdt

--- mysite/trade/views.py
+++ mysite/trade/views.py
@@ -4,6 +4,8 @@ import sys
 import os
 import uuid
 import requests
+import inspect
+import time
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 from django.http import HttpResponse
 # noinspection PyUnresolvedReferences
@@ -16,90 +18,98 @@ api_secret = os.environ['BINANCE_SECRETKEY']
 client = Client(api_key, api_secret)
 tradingview_passphase = os.environ['TRADINGVIEW_PASSPHASE']
 
-# from telegram.ext import *
-# import telegram
-# import os
-# telegram_bot_access_token = os.environ['TELEGRAM_BOT_ACCESS_TOKEN']
-# telegram_bot_chat_id = os.environ['TELEGRAM_BOT_CHAT_ID']
-# bot = telegram.Bot(token=telegram_bot_access_token)
-# bot.send_message(chat_id=telegram_bot_chat_id, text="""hi""")
-
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+
+def wrap_str(_str):
+    return "[" + _str + "]"
 
 @api_view(['GET', 'POST'])
 def webhook(request):
     body_unicode = request.body.decode('utf-8')
     precision = 2
-    # percentage = 0.95
-    percentage = 0.1
+    percentage = 0.95
+    # percentage = 0.1
     # preserve prev position exists less than
     preserve_prev_position_second = 30
-    req_id = "[" + str(uuid.uuid1()).split("-")[0] + "]"
-    print(req_id, 'received signal: ', body_unicode)
+    req_id = wrap_str(str(uuid.uuid1()).split("-")[0])
+    print(req_id, wrap_str(inspect.stack()[0][3]), 'received signal: ', body_unicode)
     if body_unicode:
         try:
             signal = json.loads(body_unicode)
             if signal['passphase'] == tradingview_passphase:
-                print(req_id, 'passphase correct')
-                withdrawAvailableUSDT = get_usdt(req_id=req_id)
-                entry = round(float(signal['entry']), precision)
-                ticker = signal['ticker']
-                side = 'SELL' if signal['order'] == 'sell' else 'BUY'
-                long_times = signal['strategy']['long']['times']
-                long_stop_loss = signal['strategy']['long']['stopLoss']
-                long_take_profit = signal['strategy']['long']['takeProfit']
-                short_times = signal['strategy']['short']['times']
-                short_stop_loss = signal['strategy']['short']['stopLoss']
-                short_take_profit = signal['strategy']['short']['takeProfit']
-                position_size = round(float(signal['position_size']), precision)
-                raw_quantity = math.floor(100 * float(withdrawAvailableUSDT) * percentage / entry) / 100
-                quantity = round(raw_quantity * int(long_times if side == 'BUY' else short_times), precision)
-                print(req_id, 'raw_quantity', raw_quantity)
-                print(req_id, 'quantity', quantity)
-                print(req_id, 'position_size', position_size)
+                print(req_id, wrap_str(inspect.stack()[0][3]), 'passphase correct')
+                signal_position_size = round(float(signal['position_size']), precision)
+                signal_symbol = signal['ticker']
                 prev_quantity = 0
                 prev_opposite_side = ''
-                position = get_position(req_id=req_id, symbol=ticker)
+                position = get_position(req_id=req_id, symbol=signal_symbol)
                 allowed_close_position = False
                 if position is not None:
                     prev_quantity = position['positionAmt']
-                    prev_opposite_side = 'SELL' if float(position['positionAmt']) > 0 else 'BUY'
+                    prev_opposite_side = 'SELL' if float(prev_quantity) > 0 else ('' if float(prev_quantity) == 0.0 else 'BUY')
                     prev_update_time = int(position['updateTime'])
                     now = datetime.now()
                     timestamp = datetime.timestamp(now) * 1000
                     # diff seconds
                     diff = (timestamp - prev_update_time) / 1000
                     allowed_close_position = True if diff > preserve_prev_position_second else False
-                if round(float(position_size), 3) == 0 and abs(float(prev_quantity)) > 0 and allowed_close_position:
-                    print(req_id, 'no position size, close prev position')
-                    close_position(req_id=req_id, symbol=ticker, side=prev_opposite_side, quantity=prev_quantity)
-                    print(req_id, 'end')
-                    return
+
+                # if signal position == 0, close position
+                # and abs(float(prev_quantity)) > 0  ?
+                if round(float(signal_position_size), 3) == 0 and allowed_close_position:
+                    print(req_id, wrap_str(inspect.stack()[0][3]), 'no position size')
+                    print(req_id, wrap_str(inspect.stack()[0][3]), 'close prev position')
+                    close_position(req_id=req_id, symbol=signal_symbol, side=prev_opposite_side, quantity=prev_quantity)
+                    print(req_id, wrap_str(inspect.stack()[0][3]), 'end')
+                    return HttpResponse('received')
+
+                print(req_id, wrap_str(inspect.stack()[0][3]), 'close prev open order')
+                cancel_all_open_order(symbol=signal_symbol)
+                time.sleep(1)
+
+                # prepare param
+                usdt = get_usdt(req_id=req_id)
+                signal_entry = round(float(signal['entry']), precision)
+                signal_side = 'SELL' if signal['order'] == 'sell' else 'BUY'
+                signal_long_times = signal['strategy']['long']['times']
+                signal_long_stop_loss = signal['strategy']['long']['stopLoss']
+                signal_long_take_profit = signal['strategy']['long']['takeProfit']
+                signal_short_times = signal['strategy']['short']['times']
+                signal_short_stop_loss = signal['strategy']['short']['stopLoss']
+                signal_short_take_profit = signal['strategy']['short']['takeProfit']
+                raw_quantity = math.floor(100 * float(usdt) * percentage / signal_entry) / 100
+                quantity = round(raw_quantity * int(signal_long_times if signal_side == 'BUY' else signal_short_times), precision)
+
+                print(req_id, wrap_str(inspect.stack()[0][3]), 'raw_quantity', raw_quantity)
+                print(req_id, wrap_str(inspect.stack()[0][3]), 'quantity', quantity)
+                print(req_id, wrap_str(inspect.stack()[0][3]), 'signal_position_size', signal_position_size)
+
 
                 # create order by signal
-                create_order(req_id, ticker, side, quantity, prev_quantity, prev_opposite_side, entry, long_stop_loss,
-                             long_take_profit, short_stop_loss,
-                             short_take_profit, precision)
+                create_order(req_id, signal_symbol, signal_side, quantity, prev_quantity, prev_opposite_side, signal_entry, signal_long_stop_loss,
+                             signal_long_take_profit, signal_short_stop_loss,
+                             signal_short_take_profit, precision)
 
                 post_data = {
-                    'symbol': ticker,
-                    'entry': entry,
-                    'side': side,
+                    'symbol': signal_symbol,
+                    'entry': signal_entry,
+                    'side': signal_side,
                 }
                 response = requests.post('http://127.0.0.1:5000/telegram', json=post_data)
                 content = response.content
-                print(req_id, 'content', content)
+                print(req_id, wrap_str(inspect.stack()[0][3]), 'content', content)
             else:
-                print(req_id, 'passphase incorrect')
+                print(req_id, wrap_str(inspect.stack()[0][3]), 'passphase incorrect')
                 #send telegram msg
                 # requests.get('http://127.0.0.1:5000/telegram')
         except:
-            print(req_id, "error:", sys.exc_info())
+            print(req_id, wrap_str(inspect.stack()[0][3]), "error:", sys.exc_info())
     else:
-        print(req_id, 'empty')
+        print(req_id, wrap_str(inspect.stack()[0][3]), 'empty')
 
+    print(req_id, wrap_str(inspect.stack()[0][3]), 'end')
     return HttpResponse('received')
 
 
@@ -109,7 +119,7 @@ def get_usdt(req_id):
     for balance in balances:
         if balance['asset'] == 'USDT':
             withdrawAvailableUSDT = balance['withdrawAvailable']
-    print(req_id, 'withdrawAvailableUSDT', withdrawAvailableUSDT)
+    print(req_id, wrap_str(inspect.stack()[0][3]), withdrawAvailableUSDT)
     return withdrawAvailableUSDT
 
 
@@ -118,26 +128,28 @@ def cancel_all_open_order(symbol):
 
 
 def get_position(req_id, symbol):
-    print(req_id, 'get_position')
+    print(req_id, wrap_str(inspect.stack()[0][3]))
     positions = client.futures_account()['positions']
     target = None
     for position in positions:
         if position['symbol'] == symbol:
             target = position
-            print(req_id, 'position', position)
-            print(req_id, 'has initial margin', float(position['initialMargin']) > 0)
-            print(req_id, 'leverage', position['leverage'])
-            print(req_id, 'quantity', position['positionAmt'])
-            print(req_id, 'opposite_side', 'SELL' if float(position['positionAmt']) > 0 else 'BUY')
+            print(req_id, wrap_str(inspect.stack()[0][3]), 'position', position)
+            print(req_id, wrap_str(inspect.stack()[0][3]), 'has initial margin', float(position['initialMargin']) > 0)
+            print(req_id, wrap_str(inspect.stack()[0][3]), 'leverage', position['leverage'])
+            print(req_id, wrap_str(inspect.stack()[0][3]), 'quantity', position['positionAmt'])
+            print(req_id, wrap_str(inspect.stack()[0][3]), 'opposite_side', 'SELL' if float(position['positionAmt']) > 0 else 'BUY')
             return target
     return None
 
 
 def close_position(req_id, symbol, side, quantity):
+    if side == '':
+        return
     precision = 3
-    print(req_id, 'close_position', symbol, side, round(float(quantity), precision))
+    print(req_id, wrap_str(inspect.stack()[0][3]), symbol, side, round(float(quantity), precision))
     if abs(float(quantity)) != 0.0:
-        print(req_id, 'has position')
+        print(req_id, wrap_str(inspect.stack()[0][3]), 'has position')
         response = client.futures_create_order(
             symbol=symbol,
             type="MARKET",
@@ -145,9 +157,9 @@ def close_position(req_id, symbol, side, quantity):
             quantity=round(abs(float(quantity)), precision),
             reduceOnly='True'
         )
-        print(req_id, 'close_position succ', response)
+        print(req_id, wrap_str(inspect.stack()[0][3]), 'succ', response)
     else:
-        print(req_id, 'no position')
+        print(req_id, wrap_str(inspect.stack()[0][3]), 'no position')
 
 
 def close_position_at_price(req_id, symbol, side, stop_price):
@@ -159,19 +171,17 @@ def close_position_at_price(req_id, symbol, side, stop_price):
         closePosition='True',
         stopPrice=stop_price
     )
-    print(req_id, response)
+    print(req_id, wrap_str(inspect.stack()[0][3]), response)
 
 
 def create_order(req_id, symbol, side, quantity, prev_quantity, prev_opposite_side, entry, long_stop_loss,
                  long_take_profit, short_stop_loss, short_take_profit,
                  precision):
-    print(req_id, 'create_order', symbol, side, quantity, entry)
+    print(req_id, wrap_str(inspect.stack()[0][3]), 'create_order', symbol, side, quantity, entry)
 
-    print(req_id, 'cancel prev open order')
-    cancel_all_open_order(symbol=symbol)
-    print(req_id, 'close prev position')
+    print(req_id, wrap_str(inspect.stack()[0][3]), 'close prev position')
     if abs(float(prev_quantity)) > 0.0:
-        print(req_id, 'has position')
+        print(req_id, wrap_str(inspect.stack()[0][3]), 'has position')
         close_position(req_id=req_id, symbol=symbol, side=prev_opposite_side, quantity=prev_quantity)
 
     stop_loss_side = 'SELL' if side == 'BUY' else 'BUY'
@@ -213,7 +223,7 @@ def create_order(req_id, symbol, side, quantity, prev_quantity, prev_opposite_si
             'reduceOnly': 'True'
         }
     ]
-    print(req_id, 'batch order', json.dumps(batch_payload), '\r\n')
+    print(req_id, wrap_str(inspect.stack()[0][3]), 'batch order', json.dumps(batch_payload), '\r\n')
     # response = client.create_test_order(
     #     symbol=symbol,
     #     side=side,
@@ -231,4 +241,4 @@ def create_order(req_id, symbol, side, quantity, prev_quantity, prev_opposite_si
     #     price=entry
     # )
     response = client.futures_place_batch_order(batchOrders=json.dumps(batch_payload))
-    print(req_id, response)
+    print(req_id, wrap_str(inspect.stack()[0][3]), response)
